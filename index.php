6ef9e87a56b49c87f448e96d56653ae5cce2dfbe
Tristan Harris <tristan@mohc.net>
2015-03-16 13:58:09 +0000

    Drupal 6.0
    commit f8d6bbf44160e6d00e71f0172ecf80e78d0f0d3c
    Author: Gábor Hojtsy <gabor@hojtsy.hu>
    Date:   Wed Feb 13 14:25:42 2008 +0000
    
        Drupal 6.0

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
  *
  * The routines here dispatch control to the appropriate handler, which then
  * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
  */
 
 require_once './includes/bootstrap.inc';
@@ -31,7 +34,6 @@ if (is_int($return)) {
 elseif (isset($return)) {
   // Print any value (including an empty string) except NULL or undefined:
   print theme('page', $return);
-
 }
 
 drupal_page_footer();
